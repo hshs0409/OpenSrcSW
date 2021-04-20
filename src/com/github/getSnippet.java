a5037629af7b77fd6699cdfa09c99145688fad76
Lee Hyun Se <0409hshs@gmail.com>
2021-04-20 18:58:04 +0900

    Update getSnippet.java

--- src/com/github/getSnippet.java
+++ src/com/github/getSnippet.java
@@ -1,10 +1,5 @@
 package com.github;
 
-import org.snu.ids.kkma.index.Keyword;
-import org.snu.ids.kkma.index.KeywordExtractor;
-import org.snu.ids.kkma.index.KeywordList;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
